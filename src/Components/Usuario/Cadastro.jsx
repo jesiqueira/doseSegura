ef11c22af7fdd846a7262be95579024879f7da14
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2024-01-29 06:20:43 -0300

    Iniciado desenvolvimento page cadastro

--- src/Components/Usuario/Cadastro.jsx
+++ src/Components/Usuario/Cadastro.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import styles from './Cadastro.module.css'
+import Label from '../Forms/Input'
+import Button from '../Forms/Button'
 
 const Cadastro = () => {
   return (
-    <div>
+    <main className={styles.main}>
       <h1>Bem Vindo(a) a DoseSegura</h1>
-    </div>
+      <div className={styles.box}>
+        <h1>Cadastrar-ser</h1>
+        <p>Tenha acesso ao sistema de controle de medicamento</p>
+        <form action="">
+          <Label name="nome" placeholder="Informe seu nome" type="text" />
+          <Label name="email" placeholder="@email.com" type="email" />
+          <Label name="password" placeholder="Senha" type="password" />
+          <Button>Enviar</Button>
+        </form>
+      </div>
+    </main>
   )
 }
 
